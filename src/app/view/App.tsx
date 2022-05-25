f2a3b02b212e1759bb85af3cd99f6cfd4464d8c4
Ivan Devat <idevat@redhat.com>
2022-05-25 14:35:25 +0200

    improve structure

---
+++ src/app/view/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          HA Cluster Management
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
